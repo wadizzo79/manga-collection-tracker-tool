bbd5952db858212bd6aa3b82a67aeb967e954331
wadizzo79 <caywadawi7@gmail.com>
2026-09-15 16:59:10 +0300

    Testing compile volume data functionality

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,12 @@ function App() {
     ? Math.max(...manga.map(manga => manga.id)) + 1
     : 1; //Calculates the next available id from existing entries
 
-  function addManga(text, volumes) {
+  function addManga(id, text, volumes) {
     const newManga = {
-      id: newId,
-      text: text,
-      volumes: volumes
+      id: newId, // Manga collection's ID
+      mangaId: id, // AniList's ID
+      text: text, // Manga title
+      volumes: volumes // AniList's general volume count
 
     }; // Creates an entry
 
@@ -23,6 +24,51 @@ function App() {
       newManga
     ]); // Adds the new entry to the existing ones
   }
+
+  const mangaInfo = [{
+    id: 12345,
+    title: "Vagabond",
+    japaneseVolumes: 37,
+
+    englishEditions: [
+      {
+        name: "VIZBIG",
+        format: "omnibus",
+        volumes: [
+          {
+            number: 1,
+            covers: [1, 2, 3]
+          },
+          {
+            number: 2,
+            covers: [4, 5, 6]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    id: 54321,
+    title: "Blue Lock",
+    japaneseVolumes: 40,
+
+    englishEditions: []
+  }
+];
+
+  const userCollection = {
+    mangaId: 12345,
+    title: "Vagabond",
+
+    englishEditions: [
+      {
+        name: "VIZBIG",
+        volumes: [1, 2]
+      }
+    ]
+  };
+
+  const selectedManga = mangaInfo.find(manga => manga.id === id);
 
   return (
     <>
--- src/components/InputArea.jsx
+++ src/components/InputArea.jsx
@@ -48,7 +48,7 @@ function InputArea({ addManga }) {
             <input type= "text" value={newEntry} onChange={(e) => setNewEntry(e.target.value)} />
             <button type="submit" onClick={searchManga}>Search</button>
             {searchResults.map(manga => (
-                <p key={manga.id}>{manga.title.romaji} <button type="submit" onClick={() => addManga(manga.title.romaji, manga.volumes)}>Add</button></p>
+                <p key={manga.id}>{manga.title.romaji} <button type="submit" onClick={() => addManga(manga.id, manga.title.romaji, manga.volumes)}>Add</button></p>
             ))}
         </>
     );
