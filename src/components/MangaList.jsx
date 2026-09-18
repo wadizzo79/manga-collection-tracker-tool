2419e2e81818e204d9e973daa75b1cba47d86ffc
wadizzo79 <caywadawi7@gmail.com>
2026-09-18 14:03:05 +0300

    Volume tracking functionality configuration continues

--- src/components/MangaList.jsx
+++ src/components/MangaList.jsx
@@ -18,11 +18,12 @@ function MangaList({ manga, setManga }){
                     const selectedEdition = manga.englishEditions.find(
                         edition => edition.name === manga.selectedEdition
                     );
+                    console.log(selectedEdition.volumes.length);
 
                     return (
                         <tr>
                             <td>{manga.text}</td>
-                            <td>N/A</td>
+                            <td>{selectedEdition.volumes.length}</td> {/* Displays the number the user owns against the numbers of the selected edition available */}
                             <td>
                                 <select 
                                     value={manga.selectedEdition}
@@ -41,6 +42,17 @@ function MangaList({ manga, setManga }){
                                             </option>
                                         ))}
                                 </select> {/* The user selects the english edition from here */}
+                                {selectedEdition.volumes.map(volume => (
+                                    <button 
+                                        key={volume.number} 
+                                        className={manga.collectedVolumes.includes(volume.number) ? "collected" : ""}
+                                        onClick={() => manga.collectedVolumes.includes(volume.number) === manga.collectedVolumes ?
+                                            manga.collectedVolumes.filter(volume.number) //Continue from here
+                                         }
+                                        >
+                                            {volume.number}
+                                    </button>
+                                ))} {/* The number of volumes available to be selected is displayed here */}
                             </td>
                             <td>{manga.volumes}</td>
                         </tr>)} // Table entry per row 
